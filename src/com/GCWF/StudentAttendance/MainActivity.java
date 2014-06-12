2481faca40019d97374a18039a8c5a14d66dc370
ChuanLeiGuo <chuanleiguo@gmail.com>
2014-06-12 14:32:13 +0800

    添加导航图片，美化界面
    添加了第一次运行时的导航图片，美化了主界面

--- src/com/GCWF/StudentAttendance/MainActivity.java
+++ src/com/GCWF/StudentAttendance/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Chuan on 6/8/14.
